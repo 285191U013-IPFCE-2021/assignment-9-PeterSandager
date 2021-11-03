3cf4f344db1088e15268cfd55f3415fd7f26b653
PeterSandager <petersandager37@gmail.com>
2021-11-03 08:47:56 +0100

    commit

--- src/search.c
+++ src/search.c
@@ -1,7 +1,19 @@
 #include "search.h"
+#include <stdio.h>
+#include <assert.h>
 #include <stdbool.h>
 
+// Recursive search function.
 bool search(int a[], int n, int x)
 {
-    return 0;
+    assert(n >= 0); // Size of array must not be negative.
+
+    if (n == 0) // Base case. If the array is of size n=0 and contains no elements x won't be found.
+        return false;
+
+    else if (n > 0 && a[n - 1] == x) // Base case. If x is present in element n of the array return 'true'.
+        return true;
+
+    else // Recursive step searching till one of the base cases is reached. If x isn't present in element n repeat the search function for n-1.
+        return search(a, n - 1, x);
 }
