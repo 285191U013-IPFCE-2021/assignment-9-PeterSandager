1fcc0cbe41254da64d9d7065f31a51f4d43a7101
PeterSandager <petersandager37@gmail.com>
2021-11-03 08:46:21 +0100

    commit

--- src/sum.c
+++ src/sum.c
@@ -1,6 +1,15 @@
 #include "sum.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
+// Recursive sum function.
 int sum(int a[], int n)
 {
-    return 0;
+    assert(n >= 0); // n must not be negative.
+
+    if (n == 0) // If n=0 the array contains no elements and the sum will of course be 0.
+        return 0;
+    else
+        return a[n - 1] + sum(a, n - 1);
 }
